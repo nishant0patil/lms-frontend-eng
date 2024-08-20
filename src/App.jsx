8300a17a11fdc409ad0a7bfdf9293a5501a3bbc9
nishant0patil <pnishant590@gmail.com>
2024-08-20 19:39:12 +0530

    Added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <Route path='/about' element ={<Aboutus />} />
       <Route path='/signup' element ={<Signup />} />
       <Route path='/signin' element ={<Signin />} />
+      <Route path='/contacts' element ={<Contact />} />
       <Route path='*' element ={<Notfound />} />
     </Routes>
   );
